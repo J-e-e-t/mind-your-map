4551f4afecf44e2b17bda13cdce249cff6e5c717
J-e-e-t <sarkarjeet13501@gmail.com>
2021-10-01 10:25:24 +0530

    added the content part
    added "why mind-map" section

--- components/content.js
+++ components/content.js
@@ -3,20 +3,38 @@ import { typography } from "@mui/system";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Image from "next/image";
+import demopic from "../public/1.png";
 
 function Content() {
   return (
     <>
-      <Container>
-        <Grid container>
-          <Grid item>
-              <a>Hello</a>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        <Grid item xs={6}>
+          <Image src={demopic} alt="demopic" width="300em" height="300em" />
+        </Grid>
+        <Grid item xs={6}>
+          <Grid
+            container
+            direction="row"
+            justify="centre"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={12} >
+              <h1> Why Mindmap</h1>
+            </Grid>
+            <Grid item xs={12}>
+              <p>lorem ipsum</p>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid container>
-         <a> world</a>
-        </Grid>
-      </Container>
+      </Grid>
     </>
   );
 }
--- pages/playground/index.js
+++ pages/playground/index.js
@@ -1,36 +1,49 @@
-import React, { useState } from 'react';
-import ReactFlow, { removeElements, addEdge } from 'react-flow-renderer';
+import React, { useState } from "react";
+import ReactFlow, { removeElements, addEdge } from "react-flow-renderer";
+import { Button } from "@mui/material";
 
 const initialElements = [
   {
-    id: '1',
-    type: 'input',
-    data: { label: 'Input Node' },
+    id: "1",
+    type: "input",
+    data: { label: "Input Node" },
     position: { x: 250, y: 25 },
   },
   {
-    id: '2',
-    data: { label: 'Another Node' },
+    id: "2",
+    data: { label: "Another Node" },
     position: { x: 100, y: 125 },
   },
 ];
 
-const Index= () => {
+const Index = () => {
   const [elements, setElements] = useState(initialElements);
   const onElementsRemove = (elementsToRemove) =>
     setElements((els) => removeElements(elementsToRemove, els));
   const onConnect = (params) => setElements((els) => addEdge(params, els));
 
+  const addNode = () =>{
+    const elm=[...elements,{
+      id: "3",
+    data: { label: "New Node" },
+    position: { x: 150, y: 125 },
+    }]
+    setElements(elm)
+  }
+
   return (
-    <div style={{ height: 400 }}>
-      <ReactFlow
-        elements={elements}
-        onElementsRemove={onElementsRemove}
-        onConnect={onConnect}
-        deleteKeyCode={46} /* 'delete'-key */
-      />
-    </div>
+    <>
+      <div style={{ height: 400 }}>
+        <ReactFlow
+          elements={elements}
+          onElementsRemove={onElementsRemove}
+          onConnect={onConnect}
+          deleteKeyCode={46} /* 'delete'-key */
+        />
+      </div>
+      <Button onClick={addNode}>add Node</Button> 
+    </>
   );
 };
 
-export default Index
+export default Index;
