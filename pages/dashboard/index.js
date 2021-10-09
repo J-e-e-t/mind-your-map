794604349d8a336408f27c65a2a479f2b1bc8fd6
J-e-e-t <sarkarjeet13501@gmail.com>
2021-10-09 20:32:44 +0530

    last commit

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/dashboard/header'
+import Content from '../../components/dashboard/content'
 
 function index() {
     return (
         <>
             <Header></Header>
+            <Content></Content>
         </>
     )
 }
